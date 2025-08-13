af08fa37f32ce4220a923e64ed1dc502e82e9d2f
Jose C Gonzalez <jcgonj1994@gmail.com>
2025-08-13 23:32:05 +0000

    First Commit: Generator card Ok

--- src/app.js
+++ src/app.js
@@ -1,11 +1,53 @@
 import "bootstrap";
 import "./style.css";
 
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const paloTop = document.querySelector("#paloTop");
+const randomNumberCard = document.querySelector("#randomNumber");
+const paloButtom = document.querySelector("#paloButtom");
+const btnGeneretor = document.querySelector("#btn-generetor");
+
+const pokerFigures = ["♦", "♥", "♠", "♣"];
+const valueCardArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "J", "Q", "K"];
+
+// funciones
+const randomValue = (arr) => arr[Math.floor(Math.random() * arr.length)];
+
+const generatorCardFuntion = () => {
+  let figuresAleartorias = randomValue(pokerFigures);
+  let valueCard = randomValue(valueCardArr);
+
+  paloTop.innerText = figuresAleartorias;
+  paloButtom.innerText = figuresAleartorias;
+  randomNumberCard.innerText = valueCard;
+
+  if (figuresAleartorias === "♦" || figuresAleartorias === "♥") {
+    paloTop.classList.add("red");
+    paloTop.classList.remove("black");
+
+    paloButtom.classList.add("red");
+    paloButtom.classList.remove("black");
+
+    randomNumberCard.classList.add("red");
+    randomNumberCard.classList.remove("black");
+  } else {
+    paloTop.classList.add("black");
+    paloTop.classList.remove("red");
+
+    paloButtom.classList.add("black");
+    paloButtom.classList.remove("red");
+
+    randomNumberCard.classList.add("black");
+    randomNumberCard.classList.remove("red");
+  }
 };
+// eventos
+window.onload = function () {
+  generatorCardFuntion();
+};
+
+btnGeneretor.addEventListener("click", generatorCardFuntion);
+
+
